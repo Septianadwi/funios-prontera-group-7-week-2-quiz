be21a982ee50eea40bb0aee6be09c35aadc1f569
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-07-15 21:30:11 +0700

    Quiz Class vs Struct

--- funios-prontera-group-7-week-2-quiz.playground/Contents.swift
+++ funios-prontera-group-7-week-2-quiz.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 /* Quiz Optional */
-func getUsername(userID: String?) -> String! {
+func getUsername(userID: String?) -> String! { // jangan dirubah
     // Check jika userID tidak nil maka return {nama kamu}. Jika userID nil maka return nil.
 }
 
@@ -15,6 +15,10 @@ if username == nil {
     print("Nama kamu terdiri dari \(username ?? "tejo".count) huruf") // Fix the error
 }
 
-
+/* Quiz Class vs Struct
+ Buatlah 1 buah struct dengan minimal 2 property.
+ Buatlah sebuah class yang berkaitan dengan struct yang kalian buat, pastikan struct tersebut menjadi property dari class ini, didalam class ini pastikan kalian mempunyai 1 buah fungsi yang menggunakan property struct yang kalian buat tersebut.
+ Buatlah sebuah child-class {inheritance} dengan tambahan satu property, gunakan property tambahan itu pada fungsi yang sama yang ada di parent-class {overriding}, dan buatlah satu fungsi tambahan yang mempunyai nama yang sama dengan tambahan variabel {overloading}.
+ */
 
 
