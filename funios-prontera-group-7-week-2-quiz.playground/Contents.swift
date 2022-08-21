636397a042677db23e0d5905474c78049614b9ea
ana.sdp <septianadwip219@gmail.com>
2022-08-22 03:25:38 +0700

    Prontera 2
    struct dan class

--- funios-prontera-group-7-week-2-quiz.playground/Contents.swift
+++ funios-prontera-group-7-week-2-quiz.playground/Contents.swift
@@ -1,31 +1,31 @@
 import UIKit
-
-let name: String? = "ana"
-/* Quiz Optional */
-func getUsername(userID: String?) -> String! { // jangan dirubah
-    // Check jika userID tidak nil maka return {nama kamu}. Jika userID nil maka return nil.
-    if userID != nil {
-        return name ?? "ana"
-    } else {
-        return nil
-    }
-}
-getUsername(userID: nil)
-
-
-let usernameNoData = getUsername(userID: nil) // jangan dirubah
-print("Namaku adalah \(usernameNoData ?? "Ana"  + " funios")")// Fix the error, pastikan outputnya harus seperti ini "Namaku adalah {nama kamu} funios". dengan tetap mempertahankan variabel usernameNoData disitu.
 //
-
-    let username = getUsername(userID: "AnyID") // jangan dirubah
-    if username != nil {
-        print("Usernamenya \(username!)") // Fix the warning without using {default value or `??`}
-        print("Usernamenya tidak ada \(username != nil)") // Should print true
-        print("Nama kamu terdiri dari \(username!.count) huruf") // Fix the error
-    }
-
-    
+//let name: String? = "ana"
+///* Quiz Optional */
+//func getUsername(userID: String?) -> String! { // jangan dirubah
+//    // Check jika userID tidak nil maka return {nama kamu}. Jika userID nil maka return nil.
+//    if userID != nil {
+//        return name ?? "ana"
+//    } else {
+//        return nil
+//    }
+//}
+//getUsername(userID: nil)
 //
+//
+//let usernameNoData = getUsername(userID: nil) // jangan dirubah
+//print("Namaku adalah \(usernameNoData ?? "Ana"  + " funios")")// Fix the error, pastikan outputnya harus seperti ini "Namaku adalah {nama kamu} funios". dengan tetap mempertahankan variabel usernameNoData disitu.
+////
+//
+//    let username = getUsername(userID: "AnyID") // jangan dirubah
+//    if username != nil {
+//        print("Usernamenya \(username!)") // Fix the warning without using {default value or `??`}
+//        print("Usernamenya tidak ada \(username != nil)") // Should print true
+//        print("Nama kamu terdiri dari \(username!.count) huruf") // Fix the error
+//    }
+//
+//
+////
 
 /* Quiz Class vs Struct
  Buatlah 1 buah struct dengan minimal 2 property.
@@ -35,3 +35,24 @@ print("Namaku adalah \(usernameNoData ?? "Ana"  + " funios")")// Fix the error, 
  */
 
 
+//belum selesai
+struct Character {
+var kebiasaan: String
+var kelebihan: String
+    
+}
+class Animal {
+    var kebiasaan: String
+    var kelebihan: String
+    
+    init(kebiasaan: String, kelebihan: String) {
+        self.kebiasaan = kebiasaan
+        self.kelebihan = kelebihan
+}
+class owl: Animal {
+    var character: Character = Character (kebiasaan: "Tidur siang", kelebihan: "Kuat melek malem")
+}
+class rusa: Animal {
+    var character: Character = Character (kebiasaan: "Lari kencang", kelebihan: "Suka jalan-jalan")
+}//
+}
